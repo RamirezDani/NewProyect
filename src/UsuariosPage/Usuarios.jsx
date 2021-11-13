3ec9a8ad46258c93e2f8aff523895f184a031c64
YaekOne <daniramirez@hotmail.com.ar>
2021-11-13 18:35:54 -0500

    Creacion seccion usuarios, del fragment

---
+++ src/UsuariosPage/Usuarios.jsx
@@ -0,0 +1,171 @@
+import React, { useState, useEffect } from 'react'
+import { actualizarDocDataBase, consultaDb, consultaUnElementoDb } from '../config/firebase';
+
+
+
+export const Usuarios = () => {
+
+
+
+    const [listaUsuarios, setListaUsuarios] = useState([])
+   
+    
+
+    const cargarUsuarios = async () => {
+
+        const listaTemporal = await consultaDb('lista-usuarios')
+        // console.log(listaTemporal[0].rol);
+
+        setListaUsuarios(listaTemporal)
+
+    }
+
+
+
+    useEffect(() => {
+
+        cargarUsuarios()
+    }, [])
+
+
+
+    const handleSelectRol = async (e,id)=>{
+              
+        const elemTemp = await consultaUnElementoDb('lista-usuarios', id)
+        
+        const email = elemTemp.email;
+        const estado = elemTemp.estado;
+        const rol = e;
+
+        const producto = {
+            email,
+            estado,
+            rol
+          }        
+
+        actualizarDocDataBase('lista-usuarios', id,producto)    
+        
+    }
+
+
+    const handleSelectEstado = async (e,id)=>{
+              
+        const elemTemp = await consultaUnElementoDb('lista-usuarios', id)        
+        const email = elemTemp.email;
+        const estado = e;
+        const rol = elemTemp.rol;
+
+        const producto = {
+            email,
+            estado,
+            rol
+          }        
+
+        actualizarDocDataBase('lista-usuarios', id,producto)    
+        
+    }
+
+
+    return (
+        <div className="container mt-3 ">
+
+            <table className="table table-striped table-bordered 
+            align-middle border-dark mt-4" >
+
+                <thead className="table-dark">
+                    <tr className="text-center">
+                        <th scope="col">#</th>
+                        <th scope="col">Usuario</th>
+                        <th scope="col">Rol</th>
+                        <th scope="col">Estado</th>
+
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                    {
+                        listaUsuarios.map((prod, index) => (
+
+                            <tr key={prod.id} className="text-center">
+                                <th scope="row" >{index + 1}</th>
+                                <td >{prod.email}</td>
+                                <td>
+
+                                    <select id="mySelectUsuariosRol"
+                                        className="form-select text-secondary border-dark"
+                                        onChange={(e,a) => handleSelectRol(e.target.value,prod.id)}>
+
+                                        {prod.rol === 'Vendedor' ?
+
+                                            <>
+                                                <option value="Vendedor">Vendedor</option>
+                                                <option value="Administrador">Administrador</option>
+                                            </> :
+                                            'No hay'}
+
+
+                                        {prod.rol === 'Administrador' ?
+
+                                            <>                                                
+                                                <option value="Administrador">Administrador</option>
+                                                <option value="Vendedor">Vendedor</option>
+
+                                            </> :
+                                            'No hay'}
+                                        
+
+                                    </select>
+
+                                </td>
+                                <td>
+
+                                    <select id="mySelectUsuariosEstado"
+                                        className="form-select text-secondary border-dark"
+                                        onChange={(e,a) => handleSelectEstado(e.target.value,prod.id)}>
+
+                                        {prod.estado === 'No Autorizado' ?
+                                            <>
+                                                <option value="No Autorizado">No Autorizado</option>
+                                                <option value="Autorizado">Autorizado</option>
+                                                <option value="Pendiente">Pendiente</option>
+                                            </>
+
+                                            : 'No hay'}
+
+                                        {prod.estado === 'Autorizado' ?
+                                            <>
+                                                <option value="Autorizado">Autorizado</option>
+                                                <option value="No Autorizado">No Autorizado</option>
+                                                <option value="Pendiente">Pendiente</option>
+                                            </>
+
+                                            : 'No hay'}
+
+                                        {prod.estado === 'Pendiente' ?
+                                            <>
+                                                <option value="Pendiente">Pendiente</option>
+                                                <option value="No Autorizado">No Autorizado</option>
+                                                <option value="Autorizado">Autorizado</option>
+                                            </>
+
+                                            : 'No hay'}
+
+
+                                    </select>
+
+
+                                </td>
+
+                            </tr>
+                        ))
+
+                    }
+                </tbody>
+
+
+
+            </table>
+        </div>
+    )
+}
